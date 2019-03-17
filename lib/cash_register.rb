29ac2d6db8c1689d21da58426b49aef6865b251a
lieby weintraub <liebahinda@gmail.com>
2019-03-17 05:05:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   # binding.pry 
   if discount != 0 
  @total = (@total * ((100.0 - @discount.to_f)/100)).to_i
-  "After the discount, the total comes to #{@total}"
+  "After the discount, the total comes to $#{@total}"
 else
   "There is no discount to apply."
 end 
