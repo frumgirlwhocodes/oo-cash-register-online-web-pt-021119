f0171596a52ab232fd96951cf167a2b0625e4124
lieby weintraub <liebahinda@gmail.com>
2019-03-17 04:49:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+require "pry"
+class CashRegister
+  attr_accessor :discount, :total
+  def initialize(discount=nil)
+    @discount=discount 
+    @total=0 
+  end 
+  def add_item(title, price, quantity=1)
+  tota_price=  self.total += price * quantity
+    @total
+  end 
+  def apply_discount
+  # binding.pry 
+  
+  
+    
+    
+  end
+  
+end 
