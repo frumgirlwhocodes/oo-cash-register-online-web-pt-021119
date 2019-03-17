abddad80537e6a3b2496eca2787f3b52f933c743
lieby weintraub <liebahinda@gmail.com>
2019-03-17 05:00:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   # binding.pry 
   if discount != 0 
   self.total * (@total - (self.total-discount.to_f/100)).to_i 
-  ""
+  "After the discount, the total comes to #{s"
   
   
     
